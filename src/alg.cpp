8f4ab38e32eef3fe76833c03bc9a2c9a53a8f455
Nict <71667497+NictX@users.noreply.github.com>
2022-03-27 22:14:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -21,36 +21,22 @@ int countPairs2(int *arr, int len, int value) {
   return count;
 }
 
-int cbinsearch(int *arr, int size, int value, int l) {
-  int r = size - 1;
-  int count = 0;
-  while (r >= l) {
-    if (arr[(l + r) / 2] == value) {
-      count++;
-      break;
-    }
-    if (arr[(l + r) / 2] < value) {
-      l = (l + r) / 2 + 1;
-    }
-    if (arr[(l + r) / 2] > value) {
-      r = (l + r) / 2 - 1;
-    }
-  }
-  if (count != 0) {
-    for (int i = (l + r) / 2 + 1; arr[i] == value && i <= size - 1; i++) {
-      count++;
-    }
-    for (int i = (l + r) / 2 - 1; arr[i] == value && i >= 0; i--) {
-      count++;
-    }
-  }
-  return count;
-}
-
 int countPairs3(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; i++) {
-    count += cbinsearch(arr, len, value - arr[i], i);
+    int l = i + 1;
+    int r = len - 1;
+    while (l < r) {
+      if (arr[(l + r) / 2] + arr[i] < value) {
+        l = (l + r) / 2 + 1;
+      } else {
+        r = (l + r) / 2;
+      }
+    }
+    while (arr[l] + arr[i] == value) {
+      count++;
+      l++;
+    }
   }
   return count;
 }
